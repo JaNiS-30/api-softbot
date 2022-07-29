db1048c7efab0ee0acd6fafef1c69688d1ac514b
Giani Rosset Pertuzatti <giani.pertuzatti@compasso.com.br>
2022-07-29 17:06:32 -0300

    Test bills

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => console.log('Servidor rodando'))
 
-app.get('/bills', async (req, res) => {
+app.get('/bills/:value', async (req, res) => {
 
-    res.status(200).send("deu boa")
+    let url = `https://compass-soft-bot.herokuapp.com/api/bills?bills=${req.params.value}`
 
+    const response = await axios.get(url)
+
+    res.status(200).send(response.data)
 })
 
