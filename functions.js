712179505b9d64e8828a2768b309801a1d3efec4
Giani Rosset Pertuzatti <giani.pertuzatti@compasso.com.br>
2022-08-01 09:48:13 -0300

    Implement Login

--- functions.js
+++
@@ -1,8 +0,0 @@
-class Funcoes {
-    verificaFormato = (palavra, formato) => {
-        if (typeof palavra === formato) return true
-        return false
-    }
-}
-
-module.exports = new Funcoes
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const axios = require('axios')
+const projects = require('./projects.json')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
@@ -8,7 +9,31 @@ app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => console.log('Servidor rodando'))
 
-app.get('/bills/', async (req, res) => {
-    res.status(200).send("ta bombando")
+app.get('/projects/:id', async (req, res) => {
+
+    let response = []
+    projects.projects.forEach(element => {
+        if (element.idcliente == req.params.id) {
+            response.push(element)
+        }
+    });
+
+    res.status(200).send(response)
 })
 
+app.get('/login/:id/', async (req, res) => {
+
+    let options = {
+        headers: {
+            'Authorization': req.headers.Authorization,
+            'Content-Type': 'application/json'
+        }
+    }
+
+
+    let url = `https://api.hubapi.com/crm/v3/objects/contacts/${req.params.id}?properties=mobilephone,email,cpf_ou_cnpj,projeto,firstname,lastname,jobtitle`
+
+    let response = await axios.get(url, options)
+
+    res.status(200).send(response.data)
+})
