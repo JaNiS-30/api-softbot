ef62df9c6fe90b52449a96462e423d470e75ef7d
Giani Rosset Pertuzatti <giani.pertuzatti@compasso.com.br>
2022-08-01 10:18:45 -0300

    Implements FS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-const projects = require('./projects.json')
+const fs = require('fs');
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
@@ -12,7 +12,11 @@ app.listen(process.env.PORT || 3000, () => console.log('Servidor rodando'))
 app.get('/projects/:id', async (req, res) => {
 
     let response = []
-    projects.projects.forEach(element => {
+
+    let result = await fs.readFileSync("./projects.json", { encoding: "utf-8" })
+    result = JSON.parse(result)
+
+    result.projects.forEach(element => {
         if (element.idcliente == req.params.id) {
             response.push(element)
         }
@@ -29,7 +33,6 @@ app.get('/login/:id/', async (req, res) => {
             'Content-Type': 'application/json'
         }
     }
-
 
     let url = `https://api.hubapi.com/crm/v3/objects/contacts/${req.params.id}?properties=mobilephone,email,cpf_ou_cnpj,projeto,firstname,lastname,jobtitle`
 
